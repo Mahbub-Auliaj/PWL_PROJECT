3a5e1e0ae6f13c941e64565f35454fb46f184885
Mahbub-Auliaj <mahbubauliarahman@gmail.com>
2022-06-06 07:56:10 +0700

    integrasi template AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AnggotaController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//Route::resource('anggota', 'AggotaController', [
+    //'names' => [
+       // 'index' => 'faq',
+        //'store' => 'faq.new',
+        
+    //]
+//]);
+
+Route::resource('anggota',AnggotaController::class);
