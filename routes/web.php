ffa5a0ff140cbe3298085cd6b7da2321c0f618ef
Mahbub-Auliaj <mahbubauliarahman@gmail.com>
2022-06-25 19:12:38 +0700

    Edit Routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnggotaController;
+use App\Http\Controllers\PengurusController;
+use App\Http\Controllers\PinjamController;
+use App\Http\Controllers\SimpanController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,12 +25,9 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-//Route::resource('anggota', 'AggotaController', [
-    //'names' => [
-       // 'index' => 'faq',
-        //'store' => 'faq.new',
-        
-    //]
-//]);
 
-Route::resource('anggota',AnggotaController::class);
+Route::resource('anggota',AnggotaController::class)->parameter('anggotum', 'anggota');
+Route::resource('pengurus',PengurusController::class);
+Route::resource('transaksi_simpan',SimpanController::class);
+Route::resource('transaksi_pinjam',PinjamController::class);
+Route::get('transaksi_simpans/cetak_pdf',[SimpanController::class,'cetak_pdf']);
